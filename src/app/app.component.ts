438f47770f5e901ac5dedb2122f496ca84c29bcc
Alexander Gunkel <alexander.gunkel@check24.de>
2022-12-16 12:10:45 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'proposal';
+export class AppComponent implements OnInit {
+  title: string|undefined;
+  subTitle: string|undefined;
+
+  ngOnInit() {
+    this.title = "Freiheitstexte"
+    this.subTitle = 'Ideen für Leipzig';
+  }
 }
