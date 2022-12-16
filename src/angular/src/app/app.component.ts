17d5d6f6459db7bd3b8ab332915b740d031c04ac
Alexander Gunkel <alexander.gunkel@check24.de>
2022-12-16 12:59:13 +0100

    wip

--- src/angular/src/app/app.component.ts
+++ src/angular/src/app/app.component.ts
@@ -5,12 +5,21 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
+
 export class AppComponent implements OnInit {
   title: string|undefined;
-  subTitle: string|undefined;
+  content: string|undefined;
+
+  proponents: string[] = []
+
+  // id should be the hash of the content.
+  id: string|undefined;
+  previous: string|undefined;
 
   ngOnInit() {
     this.title = "Freiheitstexte"
-    this.subTitle = 'Ideen für Leipzig';
+    this.proponents = ["Tick Duck", "Trick Duck", "Track Duck"]
+    this.content = "test";
+    this.id = "adkjadkjdasjd";
   }
 }
